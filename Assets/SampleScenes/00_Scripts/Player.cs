07877c5ed95026dda695af245dbcb1513719534f
AssaSquid <jerome@nebula-tales.com>
2020-02-16 20:04:34 +0000

    New level and new style + moving camera and player.

---
+++ Assets/SampleScenes/00_Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+
+    [Tooltip("In m^-1")][SerializeField] float xSpeed = 30f;
+    [Tooltip("In m")] [SerializeField] float xRange = 10f;
+    
+    [Tooltip("In m^-1")] [SerializeField] float ySpeed = 20f;
+    [Tooltip("In m")] [SerializeField] float yTopRange = 4.5f;
+    [Tooltip("In m")] [SerializeField] float yBottomRange = -7f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        transform.localRotation = Quaternion.Euler();
+    }
+
+    private void ProcessTranslation()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, yBottomRange, yTopRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+}
