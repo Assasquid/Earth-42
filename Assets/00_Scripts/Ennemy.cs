ef1de3362aaafed0c6c6f2bacdc126e2cce38366
AssaSquid <jerome@nebula-tales.com>
2020-02-23 17:20:37 +0000

    Tweaked particles, target can be hit and destroyed.

---
+++ Assets/00_Scripts/Ennemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider meshCollider = gameObject.AddComponent<MeshCollider>();
+        meshCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent; 
+        Destroy(gameObject);
+    }
+}
