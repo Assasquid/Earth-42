8b11408ff45598dfebace4efee6c3a62b7481a27
AssaSquid <jerome@nebula-tales.com>
2020-02-24 22:20:01 +0000

    Added a health system for the enemies.
    Also added an oscillation script to bring some life to the entrance of the tunnel.

---
+++ Assets/00_Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int scorePerHit = 19;
+    [SerializeField] int hits = 10;
+
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+
+    ScoreBoard scoreBoard;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider meshCollider = gameObject.AddComponent<MeshCollider>();
+        meshCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hits < 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void ProcessHit()
+    {
+        scoreBoard.ScoreHit(scorePerHit);
+        hits--; //hits = hits -1;
+                // todo consider hit FX
+    }
+
+    private void KillEnemy()
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
